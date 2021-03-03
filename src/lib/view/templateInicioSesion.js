fd52af01675b32c7f6840c4ad983e641476dbc70
Llaira <llairareyesd@gmail.com>
2021-03-03 10:37:19 -0600

    Css de template Inicio de sesion y registro OK

--- src/lib/view/templateInicioSesion.js
+++ src/lib/view/templateInicioSesion.js
@@ -1,25 +1,30 @@
 export const logIn = () => {
   const divLogIn = document.createElement('div');
   const viewLogIn = `
-    <div class="logIn">
-    <input type="image" id="back-icon" class="back" src="./assets/back.png">
+  <div class="logIn">
+  <div class="head">
+    <a class="back" href="#/">
+    <img src="./assets/back.png" id="back-icon">
+    </a>
     <img id="logo-right" src="./assets/logo-sin-letras.png">
-    </div>
-    <p id="hello-text">¡Hola! Bienvenido otra vez.
-    Ingresa tus datos para iniciar sesión.</p>
-    <div id="email-icon">
-      <img src="" alt="">
+  </div>
+    
+    <div class="text"><p id="hello-text">¡Hola! Bienvenido otra vez.
+    Ingresa tus datos para iniciar sesión.</p></div>
+    <div class="email">
+      <img src="./assets/e-mail.png" id="email-icon">
       <input type="Email" id="login-email" placeholder="Correo">
     </div>
-    <div id="password-icon">
-      <img src="" alt="">
+    <div class="password">
+      <img src="./assets/password.png" id="password-icon">
       <input type="Password" id="login-password" placeholder="Contraseña">
       </div>
 
     <button id="login-buton">Inicio Sesión</button>
-    <p id="dont-account">¿No tienes una cuenta? <a href="#/signup"><strong>Registrate aquí</strong></a></p>
+    <div class="dont-account"><p id="dont-account">¿No tienes una cuenta? <a href="#/signup"><strong>Registrate aquí</strong></a></p>
     </div>
-    <div id="container"></div>
+    <br>
+  </div>
     `;
   divLogIn.innerHTML = viewLogIn;
   const loginForm = divLogIn.querySelector('#login-buton');
