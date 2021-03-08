5767aee4da878170e3ae2cc7ab12a8598e3bd903
Llaira <llairareyesd@gmail.com>
2021-03-08 09:51:18 -0600

    ignorar commit

--- src/lib/view/templateMuro.js
+++ src/lib/view/templateMuro.js
@@ -17,7 +17,7 @@ const displayPost = (container, db) => {
       outputData.innerHTML += `
     <div class="new-channel-cont">
     <img type="image" class="icon-pencil" src="./assets/pencil.png">
-    <p id="channel-print" class="new-channel-description">${doc.data().Post}</p>
+    <div id="container-text"><p id="channel-print" class="new-channel-description">${doc.data().Post}</p></div>
     <img type="image" class="icon-moon" src="./assets/moon.png">
     <img type="image" class="icon-commentary" src="./assets/commentary.png">
     </div>
